b492f3f2ef3df04730446df392e19c7cbf72d7f7
kmxubas-dev <kmxubas.dev@gmail.com>
2025-02-27 12:00:00 +0800

    Modify - use the profile placeholder in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -37,7 +37,7 @@ class UserFactory extends Factory
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
-            'profile_photo_path' => null,
+            'profile_photo_path' => 'assets/img/placeholders/profile.png',
             'current_team_id' => null,
         ];
     }
