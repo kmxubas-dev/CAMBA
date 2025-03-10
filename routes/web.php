88ad86e1ec2dc0095d1e2e6838d5f8a9dda1d56a
kmxubas-dev <kmxubas.dev@gmail.com>
2025-03-10 22:00:00 +0800

    Implement - Web ProductPurchaseController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Web\AppController;
 use App\Http\Controllers\Web\ProductAuctionController;
 use App\Http\Controllers\Web\ProductBidController;
 use App\Http\Controllers\Web\ProductController;
+use App\Http\Controllers\Web\ProductPurchaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AppController::class, 'welcome'])->middleware('guest');
@@ -19,6 +20,7 @@ Route::middleware([
     Route::resource('products', ProductController::class);
     Route::resource('auctions', ProductAuctionController::class);
     Route::resource('bids', ProductBidController::class);
+    Route::resource('purchases', ProductPurchaseController::class);
 
     Route::post('auctions/{auction}/bid', [ProductBidController::class, 'storeOrUpdate'])
         ->name('auctions.bid');
